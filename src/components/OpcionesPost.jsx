88c1ff009f4edcdbd371588aad8197d9a632b34b
KevinFrutos <vinke.14297@gmail.com>
2022-03-24 20:54:30 +0100

    Añadido botón cerrar para pantallas menores de 800px de ancho

--- src/components/OpcionesPost.jsx
+++ src/components/OpcionesPost.jsx
@@ -18,7 +18,7 @@ const OpcionesPost = ({ user }) => {
 
 	return (
 		<>
-			<img onMouseOver={() => setIsToggle(true)} className={css.defaultButton} src={menuImgPath} alt='Menu dots' />
+			<img onClick={() => setIsToggle(true)} className={css.defaultButton} src={menuImgPath} alt='Menu dots' />
 			{isToggle ? (
 				<ul onMouseLeave={() => setIsToggle(false)} className={css.default}>
 					{!userData.followers ? (
@@ -26,15 +26,18 @@ const OpcionesPost = ({ user }) => {
 							<Link className={css.linkStyles} to='/login'>
 								<li>Seguir</li>
 							</Link>
+							<li className={css.defaultMobile} onClick={() => setIsToggle(false)}>Cerrar</li>
 						</>
 					) : userData.user === user ? (
 						<>
 							<li>Editar Post</li>
 							<li>Eliminar</li>
+							<li className={css.defaultMobile} onClick={() => setIsToggle(false)}>Cerrar</li>
 						</>
 					) : (
 						<>
 							<Follow user={user} />
+							<li className={css.defaultMobile} onClick={() => setIsToggle(false)}>Cerrar</li>
 						</>
 					)}
 				</ul>
