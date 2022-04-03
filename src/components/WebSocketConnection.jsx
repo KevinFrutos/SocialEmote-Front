a91cbc988715e2c74f78bc85d40b648915d4c23c
KevinFrutos <vinke.14297@gmail.com>
2022-04-03 21:09:04 +0200

    Añadida la funcionalidad de eliminar sesiones por WebSocket

--- src/components/WebSocketConnection.jsx
+++ src/components/WebSocketConnection.jsx
@@ -1,14 +1,20 @@
 //IMPORTS
 import { useEffect, useContext } from "react";
+import { useCookies } from "react-cookie";
 
 //CONTEXTS
 import { PublicationsDataContext } from "./contexts/PublicationsContext";
+import { IsLoggedContext } from "./contexts/IsLoggedContext";
+import { UserDataContext } from "./contexts/UserDataContext";
 
 //CONTROLLERS
 import { wsUrl } from "./controllers/httpRequests";
 
 const WebSocketConnection = () => {
 	const { setPublicaciones } = useContext(PublicationsDataContext);
+	const { updateIsLogged } = useContext(IsLoggedContext);
+	const { setUserData } = useContext(UserDataContext);
+	const [cookies, setCookie, removeCookie] = useCookies(["isLogged"]);
 
 	useEffect(() => {
 		// CADA VEZ QUE SE ABRE LA PAGINA SE CONECTA AL WEBSOCKET
@@ -21,6 +27,10 @@ const WebSocketConnection = () => {
 			//console.log(data);
 			if (data.type === "publicaciones") {
 				setPublicaciones(data.publicaciones);
+			} else if (data.type === "session") {
+				removeCookie("isLogged");
+				updateIsLogged(false);
+				setUserData({});
 			}
 		};
 	});
